cd67cd3f08ac5ca2b9d3765f725c1daf60f16953
moe <moe.faridd@gmail.com>
2020-11-25 21:31:32 -0500

    final commit

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,41 +1,16 @@
 class LocationsController < ApplicationController
-  before_action :set_location, only: [:show, :update, :destroy]
+  before_action :set_location, only: [:show]
 
   # GET /locations
   def index
     @locations = Location.all
 
-    render json: @locations
+    render json: @locations.to_json(include :temperatures)
   end
 
   # GET /locations/1
   def show
-    render json: @location
-  end
-
-  # POST /locations
-  def create
-    @location = Location.new(location_params)
-
-    if @location.save
-      render json: @location, status: :created, location: @location
-    else
-      render json: @location.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /locations/1
-  def update
-    if @location.update(location_params)
-      render json: @location
-    else
-      render json: @location.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /locations/1
-  def destroy
-    @location.destroy
+    render json: @location.to_json(include: :temperatures)
   end
 
   private
@@ -44,8 +19,4 @@ class LocationsController < ApplicationController
       @location = Location.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
-    def location_params
-      params.require(:location).permit(:lat, :lng, :name)
-    end
-end
+  end
